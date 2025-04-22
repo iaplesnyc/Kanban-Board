bd402640b60cdcd38245dea83df204015da88938
iaplesnyc <iaplesnyc@gmail.com>
2025-04-21 23:24:41 -0400

    Implement AuthService to manage token storage and user session

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -2,29 +2,39 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
-  
+
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload & { exp: number }>(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      }
+      return false;
+    } catch (err) {
+      return true;
+    }
   }
 
   getToken(): string {
-    // TODO: return the token
+    return localStorage.getItem('id_token') || '';
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/'); // Redirect to home or Kanban board
   }
 
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/login'); // Redirect to login page
   }
 }
 
